7347b09cfd2d1008d473297815417ce0175a6d7a
ice.kos <ice@kos.co.th>
2022-09-01 08:11:54 +0000

    -

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ function validateEmail(email) {
 // When the client is ready, run this code (only once)-
 client.once('ready', async () => {
     console.log('Ready!');
-    // await wellcomeMessage(client)
+    await wellcomeMessage(client)
 });
 
 
