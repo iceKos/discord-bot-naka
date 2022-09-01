8585d4f15d9fe1e27f6c06d0173514e0fdfb844b
ice.kos <ice@kos.co.th>
2022-09-01 08:03:52 +0000

    -

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ dotenv.config();
 // Require the necessary discord.js classes
 const { Client, ButtonBuilder, GatewayIntentBits, SlashCommandBuilder, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, ButtonStyle, Routes } = require('discord.js');
 const { REST } = require('@discordjs/rest');
-const { DISCORD_TOKEN, APP_ID, PUBLIC_KEY, GUILD_ID, WELLCOME_CHANNEL_ID } = process.env;
+const { DISCORD_TOKEN, APP_ID, PUBLIC_KEY, GUILD_ID, WELLCOME_CHANNEL_ID, API_NAKAMOTO } = process.env;
 const app = express()
 const port = 3000
-
+const axios = require("axios")
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -73,30 +73,51 @@ app.listen(port, () => {
                         await interaction.showModal(modal)
                     }
                 } else if (interaction.isButton()) {
-                    const modal = new ModalBuilder()
-                        .setCustomId('nakamoto-email-modal')
-                        .setTitle('Nakamoto');
 
-                    // Create the text input components
-                    const favoriteColorInput = new TextInputBuilder()
-                        .setCustomId('emailNakamoto')
-                        // The label is the prompt the user sees for this input
-                        .setLabel("What's your email in nakamoto.games ?")
-                        // Short means only a single line of text
-                        .setStyle(TextInputStyle.Short);
+                    switch (interaction.customId) {
+                        case "link_account_button": {
+                            const modal = new ModalBuilder()
+                                .setCustomId('nakamoto-email-modal')
+                                .setTitle('Nakamoto');
 
-                    // An action row only holds one text input,
-                    // so you need one action row per text input.
-                    const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+                            // Create the text input components
+                            const favoriteColorInput = new TextInputBuilder()
+                                .setCustomId('emailNakamoto')
+                                // The label is the prompt the user sees for this input
+                                .setLabel("What's your email in nakamoto.games ?")
+                                // Short means only a single line of text
+                                .setStyle(TextInputStyle.Short);
 
-                    // Add inputs to the modal
-                    modal.addComponents(firstActionRow);
-                    await interaction.showModal(modal)
+                            // An action row only holds one text input,
+                            // so you need one action row per text input.
+                            const firstActionRow = new ActionRowBuilder().addComponents(favoriteColorInput);
+
+                            // Add inputs to the modal
+                            modal.addComponents(firstActionRow);
+                            await interaction.showModal(modal)
+                            break;
+                        }
+                        default:
+                            break;
+                    }
                 } else if (interaction.isModalSubmit()) {
                     const email = interaction.fields.getTextInputValue('emailNakamoto');
 
-                    if(!validateEmail(email))return await interaction.reply({ content: `❗️ incoret email format plase try again later`, ephemeral: true });
-                    await interaction.reply({ content: `✅ Thank you to join us! <@${interaction.member.user.id}>.\n You email is \`${email}\` \n [LET PLAY GAME](https://nakamoto.games)`, ephemeral: true });
+                    if (!validateEmail(email)) return await interaction.reply({ content: `❗️ incoret email format plase try again later`, ephemeral: true });
+
+                    try {
+                        var result = await submit_link_account_with_email(email, interaction.member.user.id)
+
+                        if (result.status == true) {
+                            await interaction.reply({ content: `✅ Thank you to join us! <@${interaction.member.user.id}>.\n You email is \`${email}\` \n [LET PLAY GAME](https://nakamoto.games)`, ephemeral: true });
+                        } else {
+                            await interaction.reply({ content: `❗️ ${result.message}`, ephemeral: true })
+                        }
+                    } catch (error) {
+                        await interaction.reply({ content: error.message, ephemeral: true })
+                    }
+
+
                 }
             });
         })
@@ -107,6 +128,19 @@ app.listen(port, () => {
 
 })
 
+async function submit_link_account_with_email(email, discord_account_id) {
+    return axios.put(`${API_NAKAMOTO}/api/profile/link-profile-discord`, {
+        email: email,
+        discord_id: String(discord_account_id)
+    })
+        .then((response) => {
+            return response.data
+        })
+        .catch((error) => {
+            console.log(error);
+            throw new Error("Cannot connect to API nakamoto.game")
+        })
+}
 
 async function wellcomeMessage(_client) {
     const row = new ActionRowBuilder()
@@ -120,7 +154,7 @@ async function wellcomeMessage(_client) {
     console.log("wellcome and rule");
 }
 
-const validateEmail = (email) => {
+function validateEmail(email) {
     return String(email)
         .toLowerCase()
         .match(
@@ -129,10 +163,10 @@ const validateEmail = (email) => {
 };
 
 
-// When the client is ready, run this code (only once)
+// When the client is ready, run this code (only once)-
 client.once('ready', async () => {
     console.log('Ready!');
-    await wellcomeMessage(client)
+    // await wellcomeMessage(client)
 });
 
 
