632a94e164ba70f1810c11978968e2a806c546cf
ice.kos <ice@kos.co.th>
2022-09-09 02:59:24 +0000

    update docker-compose

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.json({
+        status:200,
+        message:"Service it working"
+    })
 })
 
 app.post("/tigger/levelup/:discord_id", async (req, res) => {
