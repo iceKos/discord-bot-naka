d81aa4903053d23c6e4a18b3cc4d95e0fb0ad8cf
ice.kos <ice@kos.co.th>
2022-09-30 08:52:52 +0000

    -

--- app.js
+++ app.js
@@ -410,18 +410,22 @@ app.listen(port, () => {
                             var guild = await client.guilds.cache.get(GUILD_ID)
 
                             var role = await guild.roles.cache.find(role => role.name === "member");
-
-                            if (role) {
-                                var member = await guild.members.cache.get(member.id) || await guild.members.fetch(member.id).catch(err => { });
-
-                                var data = await member.roles.add(role)
-                                var owner = await guild.fetchOwner()
-                                if (owner.user.id != member.id) {
-                                    if (member) {
-                                        await member.setNickname(`${member.user.username} LV ${level}`)
-                                    }
+                            if (owner.user.id != member.id) {
+                                if (member) {
+                                    await member.setNickname(`${member.user.username} LV ${level}`)
                                 }
                             }
+                            // if (role) {
+                            //     var member = await guild.members.cache.get(member.id) || await guild.members.fetch(member.id).catch(err => { });
+
+                            //     var data = await member.roles.add(role)
+                            //     var owner = await guild.fetchOwner()
+                            //     if (owner.user.id != member.id) {
+                            //         if (member) {
+                            //             await member.setNickname(`${member.user.username} LV ${level}`)
+                            //         }
+                            //     }
+                            // }
                             // prepare for reward coupon
                             await interaction.reply({ content: `✅ Thank you to join us <@${member.id}>!\nYour email is \`${email}\` \n Coupon code \`FREEITEMNAKA\`  Go to our platform to claim your rewards! [let's play games](https://www.nakamoto.games/coupon)`, ephemeral: true });
                             //await interaction.reply({ content: `✅ Thank you to join us <@${member.user.id}>!\nYour email is \`${email}\` \nGo to our platform to claim your rewards! [let's play games](https://nakamoto.games)`, ephemeral: true });
