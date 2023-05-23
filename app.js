f3d4d2d4e710e1fd3d91b57dd0598a540aa04afe
game.p <game.p@kos.co.th>
2023-05-23 11:29:15 +0000

    update rederTopplayer

--- app.js
+++ app.js
@@ -251,6 +251,7 @@ app.listen(port, () => {
 
             // register command to bot discord
             const commands = [
+                new SlashCommandBuilder().setName('create_top_player').setDescription("this command use for test create topplayer only"),
                 new SlashCommandBuilder().setName('create_event').setDescription("this command use for test create event only"),
                 new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
                 new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
@@ -386,6 +387,13 @@ app.listen(port, () => {
                             await interaction.reply({ content: "Create Event it done", ephemeral: true })
                         } catch (error) {
                             await interaction.reply({ content: "Create Event Error", ephemeral: true })
+                        }
+                    } else if (commandName === 'create_top_player') {
+                        try {
+                            await renderTopPlayer()
+                            await interaction.reply({ content: "Create top player it done", ephemeral: true })
+                        } catch (error) {
+                            await interaction.reply({ content: "Create top player Error", ephemeral: true })
                         }
                     }
                 } else if (interaction.isButton()) {
@@ -747,6 +755,37 @@ async function getCoinMarketCap(coinArray = []) {
 
     } catch (error) {
         throw new Error(error)
+    }
+}
+
+async function renderTopPlayer(data ) {
+    var rank = [{"_id":"61b0d7f5a9c230548a429347","naka_earn":23887.916222279764,"username":"Cryptodoi","avatar":"https://nakamoto-prod-new.s3.eu-central-1.amazonaws.com/naka-punk-new/8932.png"},{"_id":"635c15429d357003fd07284f","naka_earn":21275.43474227977,"username":"Mojo","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_3/f776bba25a55d3bf53181642d26812d7/image/f776bba25a55d3bf53181642d26812d7.Rank3.png"},{"_id":"61e26d3ebca1be50a6494d0f","naka_earn":12328.145851332036,"username":"Hybis","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_7/243f64b5f7316e94169161ad6ab4efd8/image/243f64b5f7316e94169161ad6ab4efd8.Rank7.png"},{"_id":"61e81f7eab90065c46f79aa5","naka_earn":11816.39871,"username":"Waen","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_17/59949370e16c7ba1e565712fd32d6901/image/7cab444d0c6030ab382e7bb5a1c58524.avatar17.png"},{"_id":"61e28933bca1be50a649a6ed","naka_earn":10771.411001332042,"username":"Chai","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_10/1573269daada86835f0a811ced27bfc3/image/1573269daada86835f0a811ced27bfc3.avatar10.png"},{"_id":"61feedff61f04a6fa627c7cd","naka_earn":10608.94503,"username":"Goodiepie","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_4/8590739e7447041975cd522cd17c39ef/image/8590739e7447041975cd522cd17c39ef.Rank4.png"},{"_id":"61fd74e461f04a6fa62260ee","naka_earn":10208.935110000002,"username":"Jannil","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_21/464ab071e51cdc365ffe9a787f11a8f2/image/464ab071e51cdc365ffe9a787f11a8f2.giphy%20%281%29.gif"},{"_id":"61b10b80ea379415e8b88f14","naka_earn":9927.239640000002,"username":"Danniej123","avatar":"assets/images/avatar/Rank2.png"},{"_id":"635c19c79d357003fd0740d1","naka_earn":8909.499240000001,"username":"Pon","avatar":"https://nakamoto-s3.s3.ap-southeast-1.amazonaws.com/setting/avatar/avatar_9/733422c3072d271daa25e15b01863c2e/image/733422c3072d271daa25e15b01863c2e.avatar9.png"},{"_id":"61b0f211c5c7011225d4cbfb","naka_earn":8741.98611,"username":"patrykoS","avatar":"https://nakamoto-prod-new.s3.eu-central-1.amazonaws.com/naka-punk-new/4171.png"}]
+    const cat_name = 'GAME-PLAYER'
+    const guild = client.guilds.cache.get(GUILD_ID)
+    var find_top_player_chanel = await guild.channels.cache.find((data) => {
+        return data.name == cat_name
+    })
+    var game_cat = null
+    if(find_top_player_chanel) {
+        game_cat = find_top_player_chanel
+    } else {
+        var cat = await guild.channels.create({
+            type : TYPE_CHANNEL.CATEGORIES,
+            name : cat_name
+        })
+        game_cat = cat
+    }
+    // console.log(game_cat)
+    // find category channel
+    var find_top_player_chanel = await guild.channels.cache.find((data) => {
+        return data.parentId == game_cat.id
+    })
+    if (!find_top_player_chanel) {
+        await guild.channels.create({
+            type: TYPE_CHANNEL.TEXT,
+            name : 'TOP PLAYER',
+            parent : game_cat.id
+        })
     }
 }
 
