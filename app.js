f60e590f98342dafd1bd2e8741f82759a67952e9
ice.kos <ice@kos.co.th>
2023-07-11 07:08:28 +0000

    open wellcome message

--- app.js
+++ app.js
@@ -896,7 +896,7 @@ function embedPlayer(data) {
 client.once('ready', async () => {
     console.log('Ready!');
     
-    // await wellcomeMessage(client)
+    await wellcomeMessage(client)
     cron.schedule('*/7 * * * *', function () {
         coinTracking().catch(console.dir);
     });
